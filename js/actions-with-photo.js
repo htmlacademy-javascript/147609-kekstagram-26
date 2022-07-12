b7a60b0a43adcf22e3e3a8824629404bfd673270
Valeria Buturlina <valeria.khlystunova@waveaccess.ru>
2022-07-12 16:39:39 +0300

    Draw me completely (part 2)

--- js/actions-with-photo.js
+++ js/actions-with-photo.js
@@ -1,29 +1,29 @@
 import { openPicture } from './big-picture-rendering.js';
 
 const pictureElementListener = (element, data) => {
-	element.addEventListener("click", function(event){
-		event.preventDefault();
-		openPicture(data);
-	}, false);
-}
+  element.addEventListener('click', (event)=> {
+    event.preventDefault();
+    openPicture(data);
+  }, false);
+};
 
 const closeDialog = () => {
-	const bigPictureDialog = document.querySelector('.big-picture');
-	bigPictureDialog.classList.add('hidden');
-	document.querySelector('body').classList.remove('modal-open');	
-}
+  const bigPictureDialog = document.querySelector('.big-picture');
+  bigPictureDialog.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+};
 
 const closeButton = document.querySelector('.big-picture__cancel');
 
-closeButton.addEventListener("click", function(event){
-	event.preventDefault();
-	closeDialog();
+closeButton.addEventListener('click', (event)=> {
+  event.preventDefault();
+  closeDialog();
 }, false);
 
-document.addEventListener('keydown', function(event){
-	if(event.key === "Escape"){
-		closeDialog();
-	}
+document.addEventListener('keydown', (event)=> {
+  if(event.key === 'Escape'){
+    closeDialog();
+  }
 });
 
 export { pictureElementListener };
--- js/thumbnail-rendering.js
+++ js/thumbnail-rendering.js
@@ -8,15 +8,14 @@ const generatedPhotos = generatePhotos();
 const picturesListFragment = document.createDocumentFragment();
 
 generatedPhotos.forEach((data) => {
-	const {url, likes, comments} = data;
-	const pictureElement = pictureTemplate.cloneNode(true);
-	const picture = pictureElement.querySelector('.picture');
-	pictureElement.querySelector('.picture__img').src = url;
-	pictureElement.querySelector('.picture__likes').textContent = likes;
-	pictureElement.querySelector('.picture__comments').textContent = comments.length;
-	picturesListFragment.appendChild(pictureElement);
-
-	pictureElementListener(picture, data);
+  const {url, likes, comments} = data;
+  const pictureElement = pictureTemplate.cloneNode(true);
+  const picture = pictureElement.querySelector('.picture');
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  picturesListFragment.appendChild(pictureElement);
+  pictureElementListener(picture, data);
 });
-  
+
 picturesListElement.appendChild(picturesListFragment);
