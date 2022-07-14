6802e1c3fb8a9761150de8a55adf2ba8b1d5f3d7
Keks <keks@htmlacademy.ru>
2022-07-14 13:06:56 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/upload-form.js
@@ -0,0 +1,95 @@
+import { isEscapeKey } from './util.js';
+
+const form = document.querySelector('.img-upload__form');
+const uploadFileElement = document.querySelector('#upload-file');
+const hashtagsField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
+const hashtagRegExp = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
+  errorTextClass: 'form__error'
+});
+
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+
+const onUploadFormEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUploadForm();
+  }
+};
+
+function closeUploadForm() {
+  uploadOverlay.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  document.removeEventListener('keydown', onUploadFormEscKeydown);
+  form.reset();
+  pristine.reset();
+}
+
+function validateHashtag(value) {
+  const hashtags = value.trim().split(' ');
+  let validate = true;
+  if(hashtags.length > 5) {
+    return false;
+  }
+  hashtags.forEach((elem, i) => {
+    if(hashtags.filter((item) => item.toLowerCase() === elem.toLowerCase()).length > 1) {
+      validate = false;
+    }
+    if(!hashtagRegExp.test(hashtags[i])) {
+      validate = false;
+    }
+  });
+
+  return validate;
+}
+
+pristine.addValidator(
+  hashtagsField,
+  validateHashtag,
+  'Некорректный #ХэшТег'
+);
+
+hashtagsField.addEventListener('input', () => {
+  pristine.validate();
+});
+
+uploadFileElement.addEventListener('change', (evt) => {
+  if(evt) {
+    uploadOverlay.classList.remove('hidden');
+    document.querySelector('body').classList.add('modal-open');
+    document.addEventListener('keydown', onUploadFormEscKeydown);
+    const cancelButton = document.querySelector('.img-upload__cancel');
+    cancelButton.addEventListener('click', (event)=> {
+      event.preventDefault();
+      closeUploadForm();
+    }, false);
+  }
+});
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    // eslint-disable-next-line no-console
+    console.log('Можно отправлять');
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('Форма невалидна');
+  }
+});
+
+hashtagsField.addEventListener('focus', () => document.removeEventListener('keydown', onUploadFormEscKeydown));
+
+hashtagsField.addEventListener('blur', () => document.addEventListener('keydown', onUploadFormEscKeydown));
+
+commentField.addEventListener('focus', () => document.removeEventListener('keydown', onUploadFormEscKeydown));
+
+commentField.addEventListener('blur', () => document.addEventListener('keydown', onUploadFormEscKeydown));
---
+++ 7/js/util.js
@@ -0,0 +1,14 @@
+const getRandomNumber = (min, max) => {
+  if(min >= 0 && max >= 0) {
+    const maxValue = max > min ? max : min;
+    const minValue = max > min ? min : max;
+    return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+  }
+  return null;
+};
+
+const checkMaxLength = (string, maxLength) => string.length <= maxLength;
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export { checkMaxLength, getRandomNumber, isEscapeKey };
