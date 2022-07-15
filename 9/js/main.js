4bdef6f2ac1bd23ba1155f575ebf2783edc0c7ff
Keks <keks@htmlacademy.ru>
2022-07-15 15:56:33 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,7 @@
+import './util.js';
+import './actions-with-photo.js';
+import './thumbnail-rendering.js';
+import './big-picture-rendering.js';
+import './upload-form.js';
+import './slider.js';
+import './image-editing.js';
