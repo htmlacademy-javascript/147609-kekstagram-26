385e47843744a4eb0ea6564a2a913f7f1cb2f629
Valeria Buturlina <valeria.khlystunova@waveaccess.ru>
2022-07-15 15:54:32 +0300

    Help a friend

---
+++ js/image-editing.js
@@ -0,0 +1,64 @@
+import { saveFilterName, changeSliderConfig, sliderValueElement } from './slider.js';
+
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const scaleControlValue = document.querySelector('.scale__control--value');
+const imgUploadPreview = document.querySelector('.img-upload__preview');
+const imgUploadEffectsFieldset = document.querySelector('.img-upload__effects');
+const sliderform = document.querySelector('.img-upload__effect-level');
+
+scaleControlValue.value = '100%';
+sliderValueElement.value = '100%';
+
+const addScaleStyle = (value) => {
+  const transformValue = value / 100;
+  imgUploadPreview.querySelector('img').style = `transform: scale(${transformValue})`;
+};
+
+const resetPhotoStyle = () => {
+  imgUploadPreview.querySelector('img').className = '';
+  imgUploadPreview.querySelector('img').style = '';
+  sliderform.classList.add('hidden');
+};
+
+const onControlSmallerClick = () => {
+  const value = parseInt(scaleControlValue.value, 10);
+  if(value > 0) {
+    scaleControlValue.value = `${value - 25}%`;
+    addScaleStyle(value - 25);
+  }
+};
+
+const onControlBiggerClick = () => {
+  const value = parseInt(scaleControlValue.value, 10);
+  if(value < 100) {
+    scaleControlValue.value = `${value + 25}%`;
+    addScaleStyle(value + 25);
+  }
+};
+
+function onFilterChange (evt) {
+  if (evt.target.matches('input[type="radio"]')) {
+    const filterValue = evt.target.value;
+
+    saveFilterName(filterValue);
+    changeSliderConfig();
+    scaleControlValue.value = '100%';
+    sliderValueElement.value = '100%';
+
+    if(filterValue !== 'none') {
+      imgUploadPreview.querySelector('img').className = `effects__preview--${evt.target.value}`;
+      sliderform.classList.remove('hidden');
+    } else {
+      resetPhotoStyle();
+    }
+  }
+}
+
+scaleControlSmaller.addEventListener('click', onControlSmallerClick);
+scaleControlBigger.addEventListener('click', onControlBiggerClick);
+
+imgUploadEffectsFieldset.addEventListener('change', onFilterChange);
+
+resetPhotoStyle();
+
--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@ import './actions-with-photo.js';
 import './thumbnail-rendering.js';
 import './big-picture-rendering.js';
 import './upload-form.js';
+import './slider.js';
+import './image-editing.js';
